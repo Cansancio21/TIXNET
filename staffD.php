00a4bcf923aafbf4bd44d9aff00b20ed03579422
Cansancio21 <ryancansancio7@gmail.com>
2025-09-06 13:13:48 +0800

    staffD pajudt

--- staffD.php
+++ staffD.php
@@ -1048,7 +1048,7 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Staff Dashboard | Ticket Reports</title>
-        <link rel="stylesheet" href="staffs.css">
+        <link rel="stylesheet" href="staffsD.css">
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
         <link rel="stylesheet" href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css">
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
