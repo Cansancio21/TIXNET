2bf3fe309fe64d9713650f58aefd969fc9e165a2
Cansancio21 <ryancansancio7@gmail.com>
2025-10-19 06:56:04 +0800

    sakto na guro na ang responsive dong

--- customersT.php
+++ customersT.php
@@ -546,7 +546,7 @@ if ($conn) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registered Customers</title>
-    <link rel="stylesheet" href="customersT.css">
+    <link rel="stylesheet" href="customerssT.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     
